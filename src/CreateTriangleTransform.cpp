0f6328117624fa7369880c21e3d7266b151037c3
aaneleh <helenakurzgon@gmail.com>
2025-04-06 12:53:22 -0300

    Instancias geradas com click

--- src/CreateTriangleTransform.cpp
+++ src/CreateTriangleTransform.cpp
@@ -1,77 +1,56 @@
 #include <iostream>
 #include <string>
 #include <assert.h>
+#include <vector>
 
 using namespace std;
 
-// GLAD
 #include <glad/glad.h>
-// GLFW
 #include <GLFW/glfw3.h>
-// GLFW
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-// Protótipo da função de callback de teclado
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode);
+using namespace glm;
 
-struct Triangle { 
-	float v1[2];
-	float v2[2];
-	float v3[2];
-	float rgb[3];
-};
+#include <cmath>
 
+struct TriangleV2 { 
+	vec3 position;
+	vec3 color;
+	GLuint VAO;
+};	
 // Protótipos das funções
 int setupShader();
 int setupGeometry(float x1, float y1, float x2, float y2, float x3, float y3);
-Triangle setupStruct(float x1, float y1, float x2, float y2, float x3, float y3, float r, float g, float b);
+TriangleV2 setupStruct(float x1, float y1, float x2, float y2, float x3, float y3, float r, float g, float b);
 
-// Dimensões da janela (pode ser alterado em tempo de execução)
 const GLuint WIDTH = 800, HEIGHT = 600;
 
-glm::mat4 matrix = glm::mat4(1);
+// Código fonte do Vertex Shader (em GLSL)
+const GLchar* vertexShaderSource = "#version 400\n"
+"layout (location = 0) in vec3 position;\n"
+"uniform mat4 projection;\n"
+"uniform mat4 model;\n"
+"void main()\n"
+"{\n"
+//...pode ter mais linhas de código aqui!
+"gl_Position = projection * model * vec4(position.x, position.y, position.z, 1.0);\n"
+"}\0";
 
-void mouse(double mx, double my) {
-	printf("mx: %d \tmy: %d", mx, my);
-    double dx = mx - WIDTH / 2;
-    double dy = my - HEIGHT / 2;
-    
-    matrix = glm::translate(glm::mat4(1), glm::vec3(dx, dy, 0));
-}
+//Código fonte do Fragment Shader (em GLSL)
+const GLchar* fragmentShaderSource = "#version 400\n"
+"uniform vec4 inputColor;\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = inputColor;\n"
+"}\n\0";
 
-// Código fonte do Vertex Shader (em GLSL): ainda hardcoded
-const GLchar *vertexShaderSource = R"(
- #version 400
- layout (location = 0) in vec3 position;
- uniform mat4 model;
- void main()
- {
-	 gl_Position = vec4(position.x, position.y, position.z, 1.0);
- }
- )";
-
-// Código fonte do Fragment Shader (em GLSL): ainda hardcoded
-const GLchar *fragmentShaderSource = R"(
- #version 400
- uniform vec4 inputColor;
- out vec4 color;
- void main()
- {
-	 color = inputColor;
- }
- )";
-
-int main()
-{
+int main(){
 	// Inicialização da GLFW
 	glfwInit();
 
-	// Muita atenção aqui: alguns ambientes não aceitam essas configurações
-	// Você deve adaptar para a versão do OpenGL suportada por sua placa
-	// Sugestão: comente essas linhas de código para desobrir a versão e
-	// depois atualize (por exemplo: 4.5 com 4 e 5)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -82,20 +61,15 @@ int main()
 
 	// Criação da janela GLFW
 	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Tarefav2 PG M2 ", nullptr, nullptr);
-	if (!window)
-	{
+	if (!window){
 		std::cerr << "Falha ao criar a janela GLFW" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
 
-	// Fazendo o registro da função de callback para a janela GLFW
-	glfwSetKeyCallback(window, key_callback);
-
 	// GLAD: carrega todos os ponteiros d funções da OpenGL
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
 		std::cerr << "Falha ao inicializar GLAD" << std::endl;
 		return -1;
 	}
@@ -115,51 +89,33 @@ int main()
 	GLuint shaderID = setupShader();
 
 	//Armazenando as posições e cores dos triangulos que serão exibidos
-	Triangle triangles[] ={
-		setupStruct(-0.5,0.0,		-0.25,0.0,		-0.5, -0.25, 	1.0, 0, 0), 
-		setupStruct(0.25,0.0,		-0.25,0.0,		 0.0, -0.25, 	0, 1.0, 0),
-		setupStruct(-0.5, -0.5, 	-0.25, -0.25,	0.0, -0.5,		0, 0, 1.0),
-		setupStruct(0.25, 0.0, 		0.5, 0.0,		0.5, -0.25,		1.0, 0, 1.0),
-		setupStruct(0.0, -0.5, 		0.5, -0.5,		0.25, -0.25,	0.0,1.0,1.0)
-	};
-
-	//Instanciando as 5 geometrias a partir das estruturas criadas acima
-	GLuint VAOs[5];
-
-	for(int i = 0 ; i < 5; i ++){
-		VAOs[i] = (GLuint)setupGeometry(triangles[i].v1[0], triangles[i].v1[1],
-			triangles[i].v2[0],triangles[i].v2[1],
-			triangles[i].v3[0],triangles[i].v3[1]
-		);
-	}  
-
-	GLuint VAOpadrao = setupGeometry(-0.1, -0.1, 0.1, -0.1, 0.0, 0.1);
+	vector<TriangleV2> triangles;
+	triangles.push_back(setupStruct(-0.5,0.0,		-0.25,0.0,		-0.5, -0.25, 	1.0, 0, 0));
+	triangles.push_back(setupStruct(0.25,0.0,		-0.25,0.0,		 0.0, -0.25, 	0, 1.0, 0));
+	triangles.push_back(setupStruct(-0.5, -0.5, 	-0.25, -0.25,	0.0, -0.5,		0, 0, 1.0));
+	triangles.push_back(setupStruct(0.25, 0.0, 		0.5, 0.0,		0.5, -0.25,		1.0, 0, 1.0));
+	triangles.push_back(setupStruct(0.0, -0.5, 		0.5, -0.5,		0.25, -0.25,	0.0,1.0,1.0));
 
 	// Enviando a cor desejada (vec4) para o fragment shader
-	// Utilizamos a variáveis do tipo uniform em GLSL para armazenar esse tipo de info
-	// que não está nos buffers
 	GLint colorLoc = glGetUniformLocation(shaderID, "inputColor");
 
 	glUseProgram(shaderID); // Reseta o estado do shader para evitar problemas futuros
 
-	double prev_s = glfwGetTime();	// Define o "tempo anterior" inicial.
-	double title_countdown_s = 0.1; // Intervalo para atualizar o título da janela com o FPS.
+	mat4 model;
+	//argumentos passados pra projeção são a área da cena, de 0 a 800 a largura, 0 à 600 altura e -1 e 1 a profundidade
+	mat4 projection = ortho(0.0, 800.0, 0.0, 600.0, -1.0, 1.0);  
+	glUniformMatrix4fv(glGetUniformLocation(shaderID, "projection"), 1, GL_FALSE, value_ptr(projection));
 
-	// Loop da aplicação - "game loop"
-	while (!glfwWindowShouldClose(window))
-	{
+
+	// Loop da aplicação 
+	while (!glfwWindowShouldClose(window)){
 
 		// Checa se houveram eventos de input (key pressed, mouse moved etc.) e chama as funções de callback correspondentes
 		glfwPollEvents();
 
-		const int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
-        if (state == GLFW_PRESS) {
-            double mx, my;
-			
-            glfwGetCursorPos(window, &mx, &my);
-            mouse(mx, my);
-			printf("mx: %f \tmy: %f", mx, my);
-        } 
+		model = mat4(1); //matriz identidade
+		model = translate(model,vec3(400.0,300.0,0.0));
+		model = scale(model,vec3(300.0, 300.0,1.0));
 
 		// Limpa o buffer de cor
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // cor de fundo
@@ -168,44 +124,41 @@ int main()
 		glLineWidth(10);
 		glPointSize(20);
 
-		for(int i = 0; i < 5; i++){
-			glBindVertexArray(VAOs[i]);
-			glUniform4f(colorLoc, triangles[i].rgb[0], triangles[i].rgb[1], triangles[i].rgb[2], 1.0f); // enviando cor para variável uniform inputColor
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-			glBindBuffer(GL_ARRAY_BUFFER, VAOs[i]);
-		}
+		glUniformMatrix4fv(glGetUniformLocation(shaderID, "model"), 1, GL_FALSE, value_ptr(model));
 
-		glBindVertexArray(VAOpadrao);
-		glUniform4f(colorLoc, ((float)rand()/(float)(RAND_MAX)), ((float)rand()/(float)(RAND_MAX)), ((float)rand()/(float)(RAND_MAX)), 1.0f); // enviando cor para variável uniform inputColor
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		glBindBuffer(GL_ARRAY_BUFFER, VAOpadrao);
+		const int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+        if (state == GLFW_PRESS) {
+            double mx, my;
+            glfwGetCursorPos(window, &mx, &my);
+			double dx = mx - WIDTH / 2;
+			double dy = -my + HEIGHT / 2;
+			
+			TriangleV2 triangle;
+			triangle.position = vec3(dx, dy, 0);
+			triangle.color = vec3(((float)rand()/(float)(RAND_MAX)), ((float)rand()/(float)(RAND_MAX)), ((float)rand()/(float)(RAND_MAX)));
+			triangle.VAO = setupGeometry((dx/500)-0.1, (dy/500)-0.1, (dx/500)+0.1, (dy/500)-0.1, (dx/500)+0.0, (dy/500)+0.1);
+			
+			triangles.push_back(triangle);
+        } 
+
+		for(int i = 0; i < triangles.size(); i++){
+			glBindVertexArray(triangles[i].VAO);
+			glUniform4f(colorLoc, triangles[i].color[0], triangles[i].color[1], triangles[i].color[2], 1.0f); // enviando cor para variável uniform inputColor
+			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glBindBuffer(GL_ARRAY_BUFFER, triangles[i].VAO);
+		}
 
 		// Troca os buffers da tela
 		glfwSwapBuffers(window);
 	}
-	// Pede pra OpenGL desalocar os buffers
-	//glDeleteVertexArrays(1, &VAO1);
-	// Finaliza a execução da GLFW, limpando os recursos alocados por ela
 	glfwTerminate();
 	return 0;
 }
 
-// Função de callback de teclado - só pode ter uma instância (deve ser estática se
-// estiver dentro de uma classe) - É chamada sempre que uma tecla for pressionada
-// ou solta via GLFW
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
-{
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
-}
 
-// Esta função está bastante hardcoded - objetivo é compilar e "buildar" um programa de
-//  shader simples e único neste exemplo de código
-//  O código fonte do vertex e fragment shader está nos arrays vertexShaderSource e
-//  fragmentShader source no iniçio deste arquivo
+//  Compilar e "buildar" um programa de shader simples e único neste exemplo de código
 //  A função retorna o identificador do programa de shader
-int setupShader()
-{
+int setupShader(){
 	// Vertex shader
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -214,24 +167,24 @@ int setupShader()
 	GLint success;
 	GLchar infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
+	if (!success){
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
 				  << infoLog << std::endl;
 	}
+
 	// Fragment shader
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 	// Checando erros de compilação (exibição via log no terminal)
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
+	if (!success){
 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
 				  << infoLog << std::endl;
 	}
+
 	// Linkando os shaders e criando o identificador do programa de shader
 	GLuint shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
@@ -239,8 +192,7 @@ int setupShader()
 	glLinkProgram(shaderProgram);
 	// Checando por erros de linkagem
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success)
-	{
+	if (!success){
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
 				  << infoLog << std::endl;
@@ -251,29 +203,17 @@ int setupShader()
 	return shaderProgram;
 }
 
-Triangle setupStruct(float x1, float y1, float x2, float y2, float x3, float y3, float r, float g, float b){
-	Triangle triangle;
-	triangle.v1[0] = y1;
-	triangle.v1[1] = x1;
-	triangle.v2[0] = y2;
-	triangle.v2[1] = x2;
-	triangle.v3[0] = y3;
-	triangle.v3[1] = x3;
-	triangle.rgb[0] = r;
-	triangle.rgb[1] = g;
-	triangle.rgb[2] = b;
+TriangleV2 setupStruct(float x1, float y1, float x2, float y2, float x3, float y3, float r, float g, float b){
+	TriangleV2 triangle;
+	triangle.position = vec3(x1, y1, 0);
+	triangle.color = vec3(r,g,b);
+	triangle.VAO = setupGeometry(x1, y1, x2, y2, x3, y3);
 	return triangle;
 }
-// Esta função está bastante harcoded - objetivo é criar os buffers que armazenam a
-// geometria de um triângulo
-// Apenas atributo coordenada nos vértices
-// 1 VBO com as coordenadas, VAO com apenas 1 ponteiro para atributo
+
+// Criar os buffers que armazenam a geometria de um triângulo
 // A função retorna o identificador do VAO
 int setupGeometry(float x1, float y1, float x2, float y2, float x3, float y3){
-	// Aqui setamos as coordenadas x, y e z do triângulo e as armazenamos de forma
-	// sequencial, já visando mandar para o VBO (Vertex Buffer Objects)
-	// Cada atributo do vértice (coordenada, cores, coordenadas de textura, normal, etc)
-	// Pode ser arazenado em um VBO único ou em VBOs separados
 	GLfloat vertices[] = {
 		// x   y     z
 		x1, y1, 0,
@@ -288,11 +228,9 @@ int setupGeometry(float x1, float y1, float x2, float y2, float x3, float y3){
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	// Envia os dados do array de floats para o buffer da OpenGl
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
 	// Geração do identificador do VAO (Vertex Array Object)
 	glGenVertexArrays(1, &VAO);
-	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
-	// e os ponteiros para os atributos
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices e os ponteiros para os atributos
 	glBindVertexArray(VAO);
 	// Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando:
 	//  Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
@@ -313,4 +251,3 @@ int setupGeometry(float x1, float y1, float x2, float y2, float x3, float y3){
 
 	return VAO;
 }
-
